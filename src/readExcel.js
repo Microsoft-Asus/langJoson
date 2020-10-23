430aacbae78fad91000228e5ae7b83b420f2338a
Archer <archer@yaudian.com>
2020-10-23 10:01:05 +0800

    test

--- src/readExcel.js
+++ src/readExcel.js
@@ -238,8 +238,8 @@ function mapping(langs, outputjson) {
       if (typeof langs[k] === 'object') {
 
         mapping( langs[k], outputjson[k]);
-      } else if (langs[k] && outputjson[k] && langs[k].indexOf('@:') == -1 && outputjson[k].indexOf('@:') == -1) {
-
+      } else if (langs[k] && outputjson[k] && langs[k].indexOf('@') == -1 && outputjson[k].indexOf('@') == -1) {
+        console.log(outputjson[k])
         delete outputjson[k];//前台前端清除
 
       }
